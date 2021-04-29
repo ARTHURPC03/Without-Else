73ece4d45ba430bc68601f670e3453fbf6bc049f
Arthur Pimentel Cordeiro <55156476+ARTHURPC03@users.noreply.github.com>
2021-04-29 13:13:34 -0300

    🐛fix: Remove old Example

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
+function netValueBetter(person) {
+  if (person?.value === null || person?.contractingRegime === null) return console.log("Algo de errado não está certo")
 
-function netValue(person) {
-  if(person?.value !== null && person?.contractingRegime !== null) {
-    if (person.contractingRegime === 'PJ') {
-      console.log("PJ")
-      console.log("6%", percentageDecrease(person.value, 6))
-    } else if (person.contractingRegime === 'CLT') {
-      console.log("CLT")
-      if(person.value <= 1903.98) {
-        console.log("0%", person.value)
-      } else if(person.value <= 2826.65) {
-        console.log("7.5%", percentageDecrease(person.value, 7.5))
-      } else if(person.value <= 3751.05) {
-        console.log("15%", percentageDecrease(person.value, 15))
-      } else if(person.value <= 4664.6) {
-        console.log("22.5%", percentageDecrease(person.value, 22.5))
-      } else {
-        console.log("27.5%", percentageDecrease(person.value, 27.5))
-      }
-    }
-  } else {
-    console.log("Algo de errado não está certo")
+  let response
+  if(person.contractingRegime === 'PJ') {
+    response = `PJ\n6% ${percentageDecrease(person.value, 6)}`
+  } 
+
+  if(person.contractingRegime === 'CLT') {
+    response = netValueCLTResponse(person)
   }
+
+  console.log(response)
+}
+
+function netValueCLTResponse(person) {
+  if(person.value <= 1903.98) return `CLT\n0% ${person.value}`
+
+  if(person.value <= 2826.65) return `CLT\n7.5% ${percentageDecrease(person.value, 7.5)}`
+  
+  if(person.value <= 3751.05) return `CLT\n15% ${percentageDecrease(person.value, 15)}`
+
+  if(person.value <= 4664.6) return `CLT\n22.5% ${percentageDecrease(person.value, 22.5)}`
+
+  return `CLT\n27.5% ${percentageDecrease(person.value, 27.5)}`
 }
 
 function percentageDecrease(value, percentage) {
@@ -28,8 +30,8 @@ function percentageDecrease(value, percentage) {
 }
 
 const person = {
-  value: 500,
-  contractingRegime: 'CLT',
+  value: 5000,
+  contractingRegime: 'PJ',
 }
 
-netValue(person)
+netValueBetter(person)
